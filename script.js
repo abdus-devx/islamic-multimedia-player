b13a9c0296e6b44dfef93679d205ba6e7b151ea5
abdus-devx <misterabdus@gmail.com>
2026-05-04 22:27:25 +0700

    feat: practice variables and data types in JS

--- script.js
+++ script.js
@@ -2,4 +2,12 @@ const playerTitle = 'Islamic Multimedia Player';
 let surahName = 'at-Thur';
 let isPlaying = true;
 
-console.log(`now playing ${surahName} on ${playerTitle} status: ${isPlaying}`);
+
+
+if (isPlaying) {
+    console.log(`now playing ${surahName}`)
+} else if (surahName) {
+    console.log(`paused : ${surahName}`)
+} else {
+    console.log('no surah selected')
+}
